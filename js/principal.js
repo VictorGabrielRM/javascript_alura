38ee0e6dcbd7c892d2e06de402789357e6cb7343
Victor <victorgabryel@hotmail.com>
2017-04-10 23:01:52 -0300

    Initial commit

---
+++ js/principal.js
@@ -0,0 +1,32 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoValido = true;
+var alturaValido = true;
+
+if (peso <= 0 || peso >= 1000){
+    console.log("Peso inválido!");
+    tdImc.textContent = "Peso inválido!";
+    pesoValido = false;
+}
+
+if (altura <= 0 || altura >= 4){
+    tdImc.textContent = "Altura inválida!!";
+    alturaValido = false;
+}
+
+if (alturaValido && pesoValido){
+     var imc = peso / (altura * altura);
+     tdImc.textContent = imc;
+}
+
